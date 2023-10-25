30b438e7aeb612f013f6a579a5018fdf38c3c130
Filipe Obrist <filipe.obrist@ua.pt>
2023-10-25 17:41:56 +0100

    Log in almost done

--- TPW_project_1/urls.py
+++ TPW_project_1/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("index/", views.index, name="index"),
     path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout", auth_views.LogoutView.as_view(next_page='/'), name="logout"),
+    path("register/", views.register, name="register"),
+    path("logout", auth_views.LogoutView.as_view(next_page='/login'), name="logout"),
     path("explore/", views.explore, name="explore"),
 
 ]
--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,10 @@
 from django import forms
 
-class LoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100)
-    password = forms.CharField(label='Password', max_length=100)
 
 class RegistForm(forms.Form):
     username = forms.CharField(label='Username', max_length=100)
-    password = forms.CharField(label='Password', max_length=100)
+    password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput)
     name = forms.CharField(label='Name', max_length=100)
-    email = forms.CharField(label='Email', max_length=100)
-    description = forms.CharField(label='Description', max_length=100)
-    image = forms.CharField(label='Image', max_length=100)
+    second_password = forms.CharField(label='Confirm Password', max_length=100, widget=forms.PasswordInput)
+
 
